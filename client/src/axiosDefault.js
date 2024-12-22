fa6a7f80c35e4a22b2dd17ace170f01adb94122e
Agus-Albarracin <abusscin_@hotmail.com>
2024-12-22 20:44:02 -0300

    m

--- client/src/axiosDefault.js
+++ client/src/axiosDefault.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosD = axios.create({
-  // baseURL: 'https://suerte24.bet/api', 
-  baseURL: 'http://localhost:3004/api',
+  baseURL: 'https://suerte24.bet/api', 
+  // baseURL: 'http://localhost:3004/api',
   timeout: 60000, //tiempo máximo de espera para las solicitudes
 });
 
